efebaa3c041756c212867ab9b8457d120f574139
Ajesh <ajesh_nair@ymail.com>
2023-03-28 17:47:46 +0530

    Intial commit

---
+++ LikeIt/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Hello World!</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ LikeIt/readme.md
@@ -0,0 +1,6 @@
+## Like It Project
+A simple project to learn and practice basics of react native ui, state, props etc.
+
+### Tasks
+[] Show a App top bar with App name Like It and a Counter with heart icon
+[] Show cards with dummy content with option to like in scrollable list of cards
